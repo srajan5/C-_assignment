26581158e6855bc8c9015db43b3cb7ca666af1d5
srajan5 <102298776+srajan5@users.noreply.github.com>
2022-04-05 20:30:30 +0530

    Update BookStruct.cs

--- BookStruct.cs
+++ BookStruct.cs
@@ -1,42 +1,101 @@
 using System;
-public class calculator
+
+public enum BookType
 {
-    public static void Main()
+    Magazine = 1,
+    Novel = 2,
+    ReferenceBook = 3,
+    Miscellaneous = 4,
+}
+
+public struct Books
+{
+    int bookId;
+    string title;
+    int    Price;
+    BookType BookType;
+
+    public int BookId
     {
-        Console.WriteLine("Enter First Number");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter Second Number");
-        int b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("The difference is {0}", a - b);
-
-        calculator.Add(a, b);
-        calculator.Subtraction(a, b);
-        calculator.Multiplication(a, b);
-        calculator.Division(a, b);
-
+        get { return bookId; } 
+        set { bookId = value; } 
     }
 
-    public static void Add(int a, int b)
+    public string Title
     {
-        int c = a + b;
-        Console.WriteLine("The Sum is {0}", c);
+        get { return title; }
+        set { title = value; }
     }
 
-    public static void Subtraction(int a, int b)
+    public int price
     {
-        int c = a - b;
-        Console.WriteLine("The Difference is {0}", c);
+        get { return price; }
+        set { price = value; }  
     }
 
-    public static void Multiplication(int a, int b)
+    public BookType bookType
     {
-        int c = a * b;
-        Console.WriteLine("The product is {0}", c);
+        get { return bookType; }
+        set { bookType = value; }
     }
 
-    public static void Division(float a, float b)
+    public static void display_details(Books bk)
     {
-        float c = a / b;
-        Console.WriteLine("The Division is {0}", c);
+
+        Console.WriteLine("The id of the book is {0}",bk.bookId);
+        Console.WriteLine("The title of the book is {0}",bk.title);
+        Console.WriteLine("The price of the book is {0}",bk.Price);
+        string type = getbooktype(bk.BookType);
+        Console.WriteLine("The title of the book is {0}",type);
     }
+
+    public static string getbooktype(BookType bookType)
+    {
+        switch(bookType)
+        {
+            case BookType.Magazine:
+                return "Magazine";
+                case BookType.Novel:
+                return "Novel";
+            case BookType.ReferenceBook:
+                return "ReferenceBook";
+            case BookType.Miscellaneous:
+                return "Miscellaneous";
+                default:
+                return "Invalid choice";
+        }
+    }
+
+    public class Book
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the book_id");
+            int id =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the book_title");
+            string title = Console.ReadLine();
+            Console.WriteLine("Please enter the book_price");
+            int price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please select the book_type - 1 for Magazine,2 for Novel,3 for ReferenceBook, 4 for Miscellaneous");
+            string type = Console.ReadLine();
+
+
+            Books b = new Books();
+            b.bookId = id;
+            b.title = title;
+            b.Price = price;
+            b.BookType = (BookType)Enum.Parse(typeof(BookType), type);
+
+            Books.display_details(b);
+
+
+        }
+       
+
+    }
+    
+
+
+
 }
