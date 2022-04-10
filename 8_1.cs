271ed53a60f3f8263d95ca0495e7930602f936e0
srajan5 <102298776+srajan5@users.noreply.github.com>
2022-04-11 00:32:07 +0530

    Add files via upload

---
+++ 8_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    public class ReflectionUsage
+    {
+        public static void ReflectionExample()
+        {
+            Assembly AS = Assembly.LoadFrom(@"F:\ConsoleApp1\ConsoleApp1\bin\Debug\net6.0\ConsoleApp1.dll");
+
+            Type[] T = AS.GetTypes();
+            foreach (Type t in T)
+            {
+                Console.WriteLine(t.FullName);
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Display the available types and their members.");
+            ReflectionExample();
+        }
+    }
+}
